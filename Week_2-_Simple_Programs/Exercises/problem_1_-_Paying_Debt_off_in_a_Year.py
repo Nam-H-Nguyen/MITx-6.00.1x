00ef2c403114cbd7b3e14a7bb27d994e130457e4
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-03-26 17:30:51 -0700

    Finish annualCreditCardDebt algoritm to figure out remaining balance after accrued annual interest rate and minimum monthly payments

--- Week_2-_Simple_Programs/Exercises/problem_1_-_Paying_Debt_off_in_a_Year.py
+++ Week_2-_Simple_Programs/Exercises/problem_1_-_Paying_Debt_off_in_a_Year.py
@@ -1,24 +1,33 @@
-from math import *
-
-# monthlyInterestRate = annualInterestRate / 12.0
-# minimumMonthlyPayment = balance - (annualInterestRate / 12)
-# monthlyUnpaidBalance = ""
-# updatedMonthlyBalance = round(5.0/4, 2)
-
-# Monthly interest rate= (Annual interest rate) / 12.0
-# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-def annualCreditDebtPayoff(balance, annualInterestRate, monthlyPaymentRate):
-    # print("Month " + str(month_counter) + " Remaining Balance: " + str(updatedMonthlyBalance))
-    # Zero Month's balance = initial balance input
-    # Payment zero (pO) = balance * minimumMonthlyPayment
+def annualCreditCardDebt(balance, annualInterestRate, monthlyPaymentRate):
+    '''
+    Input: 3 parameters - balance, annualInterestRate, monthlyPaymentRate
+    Return: A printout of the annual remaining credit card debt given a consistent monthlyPaymentRate made every month
+    '''
 
     month_counter = 0
 
+    # a while loop for 12 months or 1 year
     while month_counter < 12:
-        month_counter += 1
-        print(month_counter)
 
-print(annualCreditDebtPayoff(484, 0.2, 0.04))
+        # new balance every month will be saved here after each iteration
+        paid_minimum_monthly_balance = balance * monthlyPaymentRate
+
+        # Resave into balance variable the remaining balance after paying the minimum monthly payment
+        balance -= paid_minimum_monthly_balance
+
+        # Remaining monthly balance before interest accrued gets added to monthly accrued interest rate to get the final monthly balance
+        balance *= (1 + annualInterestRate/12)
+
+        # Keeps track of each month
+        month_counter += 1
+
+        # Credit Debt Payoff info rounded to two decimal places
+        statement_printout = "Remaining Balance: " + str(round(balance, 2))
+
+    return statement_printout
+
+# Test Case 1: \\Expected Output: 361.61 as the remaining balance
+# print(annualCreditCardDebt(484, 0.2, 0.04))
+
+# Test Case 2: \\Expected Output: 31.38 as the remaining balance
+# print(annualCreditCardDebt(42, 0.2, 0.04))
